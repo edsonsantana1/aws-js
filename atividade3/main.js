08ebd326a0fb56bc0a1c7db3dab8d8bf046778f7
Edson Santana Alves <edson_santana08@outlook.com>
2025-01-28 21:47:32 -0300

    Inclussão de nomes.

--- atividade3/main.js
+++ atividade3/main.js
@@ -1,5 +1,5 @@
 const mat = require('./math');
 
-console.log(mat.soma(1, 2));
-console.log(mat.multiplicacao(7, 2));
+console.log('A soma é:', mat.soma(1, 2));
+console.log('A multiplicação é:', mat.multiplicacao(7, 2));
 console.log('A divisão é:', mat.divisao(6, 2));
